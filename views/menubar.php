e4ff6e9a4399c5ce1807233ccd58eafff5b67f90
courtneyyaps <44823880+courtneyyaps@users.noreply.github.com>
2021-06-16 16:54:27 -0400

    menubar.php

---
+++ views/menubar.php
@@ -0,0 +1,14 @@
+<?php
+echo "<!doctype html>"
+echo "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1\" crossorigin=\"anonymous\">";
+echo "<link href='https://fonts.googleapis.com/css?family=Roboto Slab' rel='stylesheet'>";
+echo "<html lang=\"en\">";
+echo "<ul>\n";
+echo "<li><a href=\"#home\">\n";
+echo "<img id=\"logo\" src=\"../images/uvsase_darkbg_logomark-e1503005399285.png\" alt=\"UVSASE Logo\"width=25>\n";
+echo "</a></li>\n";
+echo "<li><a href=\"#home\">ELEMINI</a></li>\n";
+echo "<li style=\"float:right\"><a class=\"active\" href=\"sign_up.html\">Register</a></li>\n";
+echo "<li style=\"float:right\"><a  href=\"login.html\">Login</a></li>\n";
+echo "</ul>";
+?>
