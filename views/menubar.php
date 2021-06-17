0c4241a7e0943c2129ca4520fd620515dcae7067
courtneyyaps <44823880+courtneyyaps@users.noreply.github.com>
2021-06-16 20:25:59 -0400

    changed menubar.php

--- views/menubar.php
+++ views/menubar.php
@@ -1,8 +1,6 @@
 <?php
-echo "<!doctype html>"
 echo "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1\" crossorigin=\"anonymous\">";
 echo "<link href='https://fonts.googleapis.com/css?family=Roboto Slab' rel='stylesheet'>";
-echo "<html lang=\"en\">";
 echo "<ul>\n";
 echo "<li><a href=\"#home\">\n";
 echo "<img id=\"logo\" src=\"../images/uvsase_darkbg_logomark-e1503005399285.png\" alt=\"UVSASE Logo\"width=25>\n";
